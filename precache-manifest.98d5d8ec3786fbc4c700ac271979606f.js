1337ad3e3f0934dbf443ad5d2f720e7d3ba3f352
Roberto Achar <robertoachar@gmail.com>
2019-03-18 20:59:24 -0300

    Updates

---
+++ precache-manifest.98d5d8ec3786fbc4c700ac271979606f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "e95044c4d7c0ccd59fb6",
+    "url": "/react-fetch-abort-controller/static/js/main.76edc175.chunk.js"
+  },
+  {
+    "revision": "bfc97e99af1153a670d9",
+    "url": "/react-fetch-abort-controller/static/js/runtime~main.151cfdda.js"
+  },
+  {
+    "revision": "b43ba2d76bdc02655c76",
+    "url": "/react-fetch-abort-controller/static/js/2.67b98335.chunk.js"
+  },
+  {
+    "revision": "b52803033e79b37852f28c5711a6bffc",
+    "url": "/react-fetch-abort-controller/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/react-fetch-abort-controller/precache-manifest.98d5d8ec3786fbc4c700ac271979606f.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/react-fetch-abort-controller/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
